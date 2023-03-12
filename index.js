dd4dadab7b8104da56b379a5b9dae9d5c6920bfd
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-03-12 21:43:15 +0900

    Update index.js
    fixed bug that removes all <div with class not-period

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-
-
-
 $(".notification-num").html($(".notification[data-status='unread']").length);
 $(".notification[data-status='unread']>.notification-desc-container>div:first-child>.notification-desc").append("<div class='not-period'></div>")
 
 $(".notification[data-status='unread']").click(function(){
+    if ($(this).find(".not-period").length > 0) {
+        $(this).find(".not-period").remove();
+    }
     $(this).attr('data-status', 'read');
     $(".notification-num").html($(".notification[data-status='unread']").length);
-    $(".not-period").remove()
 });
+
 
 $('#mark-read').click(function() {
     $('.notification').attr('data-status', 'read');
