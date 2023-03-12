77d596d5d4abb093df0a267b6cbebd735c6d02fd
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-03-12 21:06:08 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 $(".notification-num").html($(".notification[data-status='unread']").length);
 $(".notification[data-status='unread']>.notification-desc-container>div:first-child>.notification-desc").append("<div class='not-period'></div>")
 
+$(".notification[data-status='unread']").click(function(){
+    $(this).attr('data-status', 'read');
+    $(".notification-num").html($(".notification[data-status='unread']").length);
+    $(".not-period").remove()
+});
+
 $('#mark-read').click(function() {
     $('.notification').attr('data-status', 'read');
     $(".notification-num").html($(".notification[data-status='unread']").length);
